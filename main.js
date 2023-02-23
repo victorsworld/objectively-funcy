537f21498e4a5862344267d613bf5887771a3158
victorsworld <victordirect48@gmail.com>
2023-02-23 15:38:21 -0500

    Finished

--- main.js
+++ main.js
@@ -1,4 +1,69 @@
 // Your code here.
+function getFirstName(m){
+return m.firstName;
+}
+function getLastName(m){
+return m.lastName;
+}
+function getFullName(m){
+return `${m.firstName} ${m.lastName}`;
+}
+function setFirstName(m, mm){
+m.firstName = mm;
+return mm;
+}
+function setAge(m, mm){
+  m.age = mm;
+return m;
+}
+function giveBirthday(m){
+  if(m.age === undefined){
+    m.age = 1;
+  } else { m.age ++;}
+  return m;
+}
+function marry(m,mm){
+if(m.married === false){
+  m.married = true
+  m.spouseName = `${mm.firstName} ${mm.lastName}`
+}
+if(mm.married === false){
+  mm.married = true
+  mm.spouseName = `${m.firstName} ${m.lastName}`
+}
+return m || mm;
+}
+
+function divorce(m,mm){
+  if(m.married === true){
+    m.married = false
+    m.spouseName = `${mm.firstName} ${mm.lastName}`
+    delete m.spouseName
+  }
+  if(mm.married === true){
+    mm.married = false
+    mm.spouseName = `${m.firstName} ${m.lastName}`
+    delete mm.spouseName
+  }
+  return m || mm;
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
